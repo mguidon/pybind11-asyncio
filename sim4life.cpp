6c30297338cf22dd20bb2f767ca0a2b9c9f5cd8d
Manuel Guidon <guidon@itis.swiss>
2020-03-12 13:29:14 +0100

    Add different versions

--- sim4life.cpp
+++ sim4life.cpp
@@ -7,31 +7,59 @@
 
 namespace py = pybind11;
 
-int wait(const int seconds)
+template<typename T>
+T wait(const T seconds)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(seconds));   
-    return 0;
+    int ms = static_cast<int>(seconds*1000);
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));   
+    return static_cast<T>(2)*seconds;
 }
 
-std::future<int> wait_async(const int seconds)
+template <> 
+std::string wait(const std::string seconds) 
+{ 
+    std::this_thread::sleep_for(std::chrono::milliseconds(1042));   
+    return "Bazinga";
+} 
+
+template<typename T>
+std::future<T> wait_async(const T seconds)
 {
-    return std::async(std::launch::async, wait, seconds);
+    return std::async(std::launch::async, wait<T>, seconds);
 }
 
 bool IsFutureReady(std::future<int>* self)
 {
     auto status = self->wait_for(std::chrono::milliseconds(10));
     return status == std::future_status::ready;
-
 }
+
+template<typename T>
+bool Ready(std::future<T>* self)
+{
+    auto status = self->wait_for(std::chrono::milliseconds(10));
+    return status == std::future_status::ready;
+}
+
+template<typename T>
+void declare_future(py::module &m, const std::string &typestr) {
+    using Future = std::future<T>;
+    std::string pyclass_name = std::string("Future") + typestr;
+    py::class_<Future>(m, pyclass_name.c_str())
+    .def(py::init<>())
+    .def("get",  &Future::get)
+    .def("ready", &Ready<T>);
+
+    m.def("wait_async", &wait_async<T>, "does some stuff asynch");
+    m.def("wait", &wait<T>, "does some stuff synch");
+}
+
 PYBIND11_MODULE(sim4life, m)
 {   
-    py::class_<std::future<int> >(m, "FutureInt")
-    .def(py::init<>())
-    .def("get", &std::future<int>::get, "gets the value of the future for the task. this call is blocking, so waits until task is done")
-    .def("ready",&IsFutureReady);
+    declare_future<float>(m, "Float");
+    declare_future<double>(m, "Double");
+    declare_future<int>(m, "Int");
+    declare_future<std::string>(m, "String");
 
     m.doc() = "sim4life plugin";
-    m.def("wait", &wait, "does some stuff synchronously");
-    m.def("wait_async", &wait_async, "does some stuff asynchronously");
 }
--- sim4life.py
+++ sim4life.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+import string
 
 import sim4life
 
@@ -7,10 +8,12 @@ import sim4life
 async def wait_for_sim4life(f, id):
     while(not f.ready()):
         await asyncio.sleep(0.1)
-    print(f"sim4life task {id} done")
+    print(f"sim4life task {id} done, result: {f.get()}")
 
 async def main():
     tasks = [wait_for_sim4life(sim4life.wait_async(random.randint(1,5)), i) for i in range(10)]
+    tasks += [wait_for_sim4life(sim4life.wait_async(random.uniform(1,5)), i) for i in range(10)]
+    tasks += [wait_for_sim4life(sim4life.wait_async(random.choice(string.ascii_lowercase)), i) for i in range(10)]
     await asyncio.gather(*tasks)
     
 
